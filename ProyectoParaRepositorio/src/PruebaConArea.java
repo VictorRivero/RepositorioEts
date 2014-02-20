e3dafc06617e853da8cf90678e56c94b68ba9196
usuario2 <usuario2@gmail.com>
2014-02-20 13:34:20 +0000

    Prueba2 cambio de altura y area iniciales

--- ProyectoParaRepositorio/src/PruebaConArea.java
+++ ProyectoParaRepositorio/src/PruebaConArea.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
+
+
 
 public class PruebaConArea {
+
+
+	private int altura=40;
+	private int base=50;
+
 	
-	
+		
+	public void Area(int altura, int base) {
+		this.altura = altura;
+		this.base = base;
+	}
+
+
+	public double area(){
+		return (altura * base)/(double)2;
+	}
+
+
+	public  void main(String args[]){
+		
+		double base , altura , area;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("base : ");
+		base = sc.nextDouble();
+		System.out.println("altura : ");
+		altura = sc.nextDouble();
+		area = area(base, altura);
+		System.out.println("Area :" + area);
+		
+		sc.close();
+		
+	}
+
+
+	public double area(double base, double altura) {
+		double area;
+		area=(base*altura)/2;
+		return area;
+	}
+
 
 }
